9331467d9c48840a3205e6fc85664a275c26da91
aarushi2146 <106542499+aarushi2146@users.noreply.github.com>
2025-03-19 15:39:28 +0530

    Delete IfElseProblem directory

--- IfElseProblem/HeadTails.js
+++
@@ -1,8 +0,0 @@
-// Function to simulate a coin flip
-function flipCoin() {
-    let result = Math.random() < 0.5 ? "Heads" : "Tails";
-    console.log("Coin Flip Result:", result);
-}
-
-// Run the coin flip
-flipCoin();
